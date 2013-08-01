290bcd8b9633243bd9dc6d259ebec000761d1339
Roy Smeding <roy.smeding@gmail.com>
2013-08-01 14:45:16 +0200

    Omgekat naar LWP, werkst

--- ledbannerc3mon.pl
+++ ledbannerc3mon.pl
@@ -3,14 +3,36 @@
 use strict;
 use warnings;
 use JSON qw( decode_json );
-use File::Fetch;
+use LWP;
+use LWP::UserAgent;
+use LWP::Simple;
+use URI::Escape;
+
+print "Requesting JSON...";
 
 # Grab the latest stats from c3netmon and decode the data
-my $statsjson = File::Fetch->new(uri => 'http://c3netmon.camp.ohm2013.org/current.json');
-
-print $statsjson;
+my $statsjson = get 'http://c3netmon.camp.ohm2013.org/current.json';
 my $decoded = decode_json($statsjson);
 
-
 # Pulling out the shadow temprature from the decoded json
-print "Shadow temprature: " . $decoded->{'OHM1'}{'value'} . "\n";
+my $bannerstr = "Temperature: " . ($decoded->{'OHM1'}{'value'}[0]+273) . "K\n";
+
+# Create a user agent object
+my $ua = LWP::UserAgent->new;
+$ua->agent("https://github.com/OctopusKat/ledbannerc3mon/");
+
+my $req = HTTP::Request->new(POST => 'http://ledbanner.ip6.nl/cgi-bin/ledbanner.cgi');
+$req->content_type('application/x-www-form-urlencoded');
+$req->content('text='.uri_escape($bannerstr));
+
+print "Sending to LED banner... ";
+
+# Pass request to the user agent and get a response back
+my $res = $ua->request($req);
+
+# Check the outcome of the response
+if ($res->is_success) {
+    print "Success!\n";
+} else {
+    print $res->status_line, "\n";
+}
